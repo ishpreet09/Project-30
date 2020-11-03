a4587c96f491602d2c56eb4edf38888c20f7ef12
ishpreet09 <71171946+ishpreet09@users.noreply.github.com>
2020-11-03 08:57:43 +0530

    Add files via upload

--- js/Block.js
+++ js/Block.js
@@ -10,16 +10,29 @@ class Block{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
+        this.Visibility=255;
         World.add(world, this.body);
       }
       display(){
-        var angle = this.body.angle;
-        var pos= this.body.position;
+        
+        if (this.body.speed<5) {
+         var angle = this.body.angle;
+        var pos= this.body.position; 
         push();
         translate(pos.x, pos.y);
         rotate(angle);
         rectMode(CENTER);
         rect(0,0,this.width, this.height);
         pop();
+        }
+        else {
+          World.remove(world,this.body);
+          push();
+          this.Visibility=this.Visibility-5;
+          tint(255,this.Visibility);
+          //rect(this.body.position.x,this.body.position.y,30,40);
+          pop();
+        }
+        
       }
 }
